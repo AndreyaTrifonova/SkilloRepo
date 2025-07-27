74d695965199ba19276d724dd04e215054368b5e
AndreyaTrifonova <andreyatrifonova@gmail.com>
2025-07-27 11:13:32 +0300

    commit

---
+++ src/Exercise1/NamePrinter.java
@@ -0,0 +1,10 @@
+package Exercise1;
+
+public class NamePrinter {
+    public static void main(String[] args) {
+        String firstName = "Andreya";
+        String middleName = "Stiliyanova";
+        String lastName = "Trifonova";
+        System.out.println(firstName + " " + middleName + " " + lastName);
+    }
+}
