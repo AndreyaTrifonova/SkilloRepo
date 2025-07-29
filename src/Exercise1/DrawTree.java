672d32af3fee1870ae561474bd79c17aa137424e
AndreyaTrifonova <andreyatrifonova@gmail.com>
2025-07-29 19:26:21 +0300

    commit

---
+++ src/Exercise1/DrawTree.java
@@ -0,0 +1,11 @@
+package Exercise1;
+
+public class DrawTree {
+    public static void main(String[] args) {
+        System.out.println("   *");
+        System.out.println("  ***");
+        System.out.println(" *****");
+        System.out.println("   *");
+    }
+}
+
